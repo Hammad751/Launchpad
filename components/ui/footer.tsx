0cd8d1b8017884dc3657e81e53d6f4b14d8a034f
Hammad <hammadzia234@gmail.com>
2025-08-25 16:24:19 +0500

    update the resposiveness of ui

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -7,52 +7,47 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import { BsTwitterX } from "react-icons/bs";
 const Footer = () => {
     return (
-        <div className='flex justify-around p-8 bg-[#f5f5f5]'>
+        <>
+           <div className="min-h-[5rem] flex flex-col md:flex-row justify-around p-8 bg-[#f5f5f5]">
 
-            <div>
-                <Image src="/logo.svg" alt="VRCN Logo" width={150} height={150} />
-                <div className="flex items-baseline">
-                <h1 className="text-2xl sm:text-xl mx-2 md:text-3xl font-bold">
-                  VRCN
-                </h1>
-                <h2 className="text-xl sm:text-lg md:text-2xl font-bold text-transparent bg-clip-text text-[burlywood]">
-                  Launchpad
-                </h2>
-              </div>
+                {/* <div className="relative z-10"> */}
+                    <div>
+                        <Image src="/logo.svg" alt="VRCN Logo" width={150} height={150} />
+                        <div className="flex items-baseline">
+                        <h1 className="text-2xl sm:text-xl mx-2 md:text-3xl font-bold">
+                        VRCN
+                        </h1>
+                        <h2 className="text-xl sm:text-lg md:text-2xl font-bold text-transparent bg-clip-text text-[#be9c6e]">
+                        Launchpad
+                        </h2>
+                    </div>
 
-            </div>  
-            <div className='flex  flex-col'>
-                {/* <div className=' '>
-                    About
-                </div> */}
-                <h6 className=' text-xl font-bold'>
-                    Support
-                </h6>
+                    </div>  
+                    <div className='flex  flex-col'>
+                        <h6 className=' text-xl font-bold'>
+                            Support
+                        </h6>
 
-                <ul className='mt-3'>
-                    <li>
-                        FAQ
-                    </li>
-                    <li>
-                        Contact Us
-                    </li>
-                </ul>
+                        <div className='flex gap-3 mt-3'>
+                            <a href='https://t.me/+xG1JjT-3EPIzZjNk'><FaTelegram size={30} /></a>
+                        </div>
 
+                    </div>
+
+                    <div> 
+                        <h6 className=' text-xl font-bold'>
+                            Social Media
+                        </h6>
+                        <div className='flex gap-3 mt-3'>
+                            <a href='https://x.com/VRCNOfficial'><BsTwitterX size={30}/></a>
+                            <a href='https://www.facebook.com/officialvrcncoin'><FaSquareFacebook size={30} /></a>
+                            <a href='https://www.instagram.com/vrcn.coin/'><FaInstagram size={30}/></a>
+                            <a href='https://whatsapp.com/channel/0029VaECVXeF6smrq8uggj1r'><IoLogoWhatsapp size={30}/></a>
+                        </div>
+                    </div>
+                {/* </div> */}
             </div>
-
-            <div>
-                <h6 className=' text-xl font-bold'>
-                    Social Media
-                </h6>
-                <div className='flex gap-3 mt-3'>
-                    <a href='https://x.com/VRCNOfficial'><BsTwitterX size={30}/></a>
-                    <a href='https://www.facebook.com/officialvrcncoin'><FaSquareFacebook size={30} /></a>
-                    <a href='https://t.me/+xG1JjT-3EPIzZjNk'><FaTelegram size={30} /></a>
-                    <a href='https://www.instagram.com/vrcn.coin/'><FaInstagram size={30}/></a>
-                    <a href='https://whatsapp.com/channel/0029VaECVXeF6smrq8uggj1r'><IoLogoWhatsapp size={30}/></a>
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
